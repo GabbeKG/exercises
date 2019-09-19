499d9204a40d917a825dab6e5eb540ecb2f15eaa
Gabriel Karlsson Gonzalez <el_sueco91@hotmail.com>
2019-09-19 14:28:02 +0200

    Lösning Övning 3 saga

---
+++ exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Mata in namn på karaktär ett:");
+                    string namn1 = Console.ReadLine();
+                    Console.WriteLine("Skriv in namn på karaktär 2: ");
+                    string namn2 = Console.ReadLine();
+
+                    Console.WriteLine(namn1 +" och "+ namn2+ " gick till en bar.");
+                    Console.WriteLine(namn1 + " tog en öl och "+namn2+" tog en flaska absint");
+                    Console.ReadLine();
+        }
+    }
+}
