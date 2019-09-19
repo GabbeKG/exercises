60a21d76ef8237e554c938b2444c65049cc7e5e4
Gabriel Karlsson Gonzalez <el_sueco91@hotmail.com>
2019-09-19 14:05:50 +0200

    Övning 2 (decimal till heltal)

---
+++ exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    int heltal;
+                    Console.WriteLine("skriv in ett decimal tal");
+                    string str = Console.ReadLine();
+                    float dec = (float)Convert.ToDouble(str);
+                    heltal = Convert.ToInt32(dec);
+
+                    Console.WriteLine("Närmsta heltal är: "+ heltal);
+                    Console.ReadLine();
+        }
+    }
+}
