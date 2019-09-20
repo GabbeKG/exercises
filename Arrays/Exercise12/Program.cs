5e68e68556309afe99b722328e2cbb316793837e
Gabriel Karlsson Gonzalez <el_sueco91@hotmail.com>
2019-09-20 15:19:12 +0200

    gitignore update + array övning(ejklar)

---
+++ Arrays/Exercise12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+
+            Console.WriteLine("Hej skriv in fem namn.");
+
+            Console.WriteLine("Namn 1: ");
+            namn[0] = Console.ReadLine();
+            Console.WriteLine("Namn 2: ");
+            namn[1] = Console.ReadLine();
+            Console.WriteLine("Namn 3: ");
+            namn[2] = Console.ReadLine();
+            Console.WriteLine("Namn 4: ");
+            namn[3] = Console.ReadLine();
+            Console.WriteLine("Namn 5: ");
+            namn[4] = Console.ReadLine();
+
+            for(int i = 0; i<namn.Length; i++){
+            Console.WriteLine(namn[i]);
+                
+            }
+            Console.WriteLine("byt namn")
+            Console.ReadLine();
+        }
+    }
+}
