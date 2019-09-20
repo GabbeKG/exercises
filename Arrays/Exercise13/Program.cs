fb426c433bd4b57a5c9c034d0bdc7c011706f549
Gabriel Karlsson Gonzalez <el_sueco91@hotmail.com>
2019-09-20 11:17:53 +0200

    Övning 5 och 13(array) lösta

---
+++ Arrays/Exercise13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] siffror = new int[5];
+
+            Console.WriteLine("skriv in 5 tal.");
+            Console.Write("tal 1: ");
+            string numEtt = Console.ReadLine();
+            siffror[0] = Convert.ToInt32(numEtt);
+            
+            Console.Write("tal 2: ");
+            string numTvå = Console.ReadLine();
+            siffror[1] = Convert.ToInt32(numTvå);
+            
+            Console.Write("tal 3: ");
+            string numTre = Console.ReadLine();
+            siffror[2] = Convert.ToInt32(numTre);
+            
+
+            Console.Write("tal 4: ");
+            string numFyra = Console.ReadLine();
+            siffror[3] = Convert.ToInt32(numFyra);
+            
+            Console.Write("tal 5: ");
+            string numFem = Console.ReadLine();
+            siffror[4] = Convert.ToInt32(numFem);
+            
+
+                int sum=0;
+            for(int i = 0; i<siffror.Length; i++){
+                sum = sum+siffror[i];
+                //Console.WriteLine("inlagd siffra: " +siffror[i]);
+            }
+               
+                Console.WriteLine("summan: "+sum);
+                 Console.ReadLine();
+        }
+    }
+}
---
+++ exercise5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mata in sitt namn.
+            Console.WriteLine("Skriv ditt namn");
+            string namn = Console.ReadLine();
+            // hälsar du användaren välkommen.
+            // användaren mata in sin ålder 
+            Console.WriteLine("Var hälsad "+ namn+"!\n När är du född?(YYYY-MM-DD");
+            // och spara undan den i lämplig variabel.
+            string ålder = Console.ReadLine();
+            DateTime år = DateTime.Parse(ålder);
+            TimeSpan årdagar= DateTime.Now.Subtract(år);
+            // Räkna sedan ut antalet dagar levt vid angiven ålder.
+            //for(int i=0; i<år)
+            // Presentera resultat.
+            int dagar = Convert.ToInt32(årdagar.TotalDays);
+            Console.WriteLine("Du är "+ dagar+" dagar gammal!");
+            Console.ReadLine();
+        }
+    }
+}
